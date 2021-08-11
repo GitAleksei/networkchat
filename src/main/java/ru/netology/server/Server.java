d335c3f11e8b96f50adaa44737caa5bbc4af62dc
Kiva Aleksey <adolfp@mail.ru>
2021-08-11 15:58:36 +0000

    Wrote Server.java

--- src/main/java/ru/netology/server/Server.java
+++ src/main/java/ru/netology/server/Server.java
@@ -1,8 +1,26 @@
 package ru.netology.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+
 public class Server {
     public Server() {
         Logger.INSTANCE.log("Start server!");
-        int port = Settings.PORT;
+
+        try (ServerSocket serverSocket = new ServerSocket(Settings.PORT)) {
+            while (true) {
+                try (Socket socket = serverSocket.accept()) {
+                    new ClientHandlingThread(socket);
+                } catch (IOException ex) {
+                    Logger.INSTANCE.log(Arrays.toString(ex.getStackTrace()) + " " + ex.getMessage());
+                }
+            }
+        } catch (IOException ex) {
+            Logger.INSTANCE.log(Arrays.toString(ex.getStackTrace()) + " " + ex.getMessage());
+        }
+
+        Logger.INSTANCE.log("Stop server!");
     }
 }
