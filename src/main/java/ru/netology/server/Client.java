fa18e3fb89fb031c6599f01f412cbe5881ef61e1
Kiva Aleksey <adolfp@mail.ru>
2021-08-12 10:05:53 +0000

    Added Client.java

---
+++ src/main/java/ru/netology/server/Client.java
@@ -0,0 +1,25 @@
+package ru.netology.server;
+
+public class Client {
+    private final int id;
+    private String name;
+    private final ClientThread clientThread;
+
+    public Client(int id, String name, ClientThread clientThread) {
+        this.id = id;
+        this.name = name;
+        this.clientThread = clientThread;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ClientThread getClientThread() {
+        return clientThread;
+    }
+}
